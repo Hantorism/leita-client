0e20a8b0126c43e338570d64b2d4b8a239bbe2ba
tae1231 <sandy2528@naver.com>
2025-02-05 23:00:17 +0900

    tailwindcss 설치

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // HTML 파일에서 Tailwind 클래스 적용
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
