38af5f531767af15517b4ea22951dd6774cb7dd5
tae1231 <sandy2528@naver.com>
2025-02-13 14:33:16 +0900

    feat: img 추가

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../common/Header";
 import { Footer } from "../common/Footer";
 // import './Home.css';
 
 const Home = () => {
+    // const [currentImage, setCurrentImage] = useState(0);
+    // const images = [
+    //     "/image/Comment.png",
+    //     "/image/Comment%20(1).png",
+    //     "/image/Component%206.png"
+    // ];
+    //
+    // useEffect(() => {
+    //     const timeout1 = setTimeout(() => {
+    //         setCurrentImage(1);
+    //     }, 1000);
+    //
+    //     const timeout2 = setTimeout(() => {
+    //         setCurrentImage(2);
+    //     }, 2000);
+    //
+    //     return () => {
+    //         clearTimeout(timeout1);
+    //         clearTimeout(timeout2);
+    //     };
+    // }, []);
+
     return (
         <div className="flex flex-col items-start h-screen text-white pl-[10%] pr-[10%] pt-[5%]">
             {/* Header */}
@@ -12,17 +34,16 @@ const Home = () => {
             </header>
 
             <div className="flex flex-col items-start justify-center w-[80%] max-w-[600px] p-5 pl-[6%] pt-[8%]">
-                <div
-                    className="inline-flex items-center gap-1 bg-white bg-opacity-10 px-3 py-2 rounded-[60px] w-fit mb-5">
-                    <img src="/image/Icon.svg" className="w-[24px] h-[24px]"/>
+                <div className="inline-flex items-center gap-1 bg-white bg-opacity-10 px-3 py-2 rounded-[60px] w-fit mb-5">
+                    <img src="/image/Icon.svg" className="w-[24px] h-[24px]" />
                     <a className="font-lexend text-sm font-light text-[#E0E0E0] leading-[1.5]">
                         다양한 프로그래밍 문제를 풀고, 실시간 온라인 채점을 통해 실력을 확인하세요.
                     </a>
                 </div>
                 <div className="text-left">
-                    <h1   className=" text-[2.5rem] font-sans font-extrabold leading-[1.3] uppercase text-white">
-                        START <br/>
-                        YOUR CODING JOURNEY<br/>
+                    <h1 className="text-[2.5rem] font-sans font-extrabold leading-[1.3] uppercase text-white">
+                        START <br />
+                        YOUR CODING JOURNEY<br />
                         WITH LEITA! 🏁
                     </h1>
                 </div>
@@ -31,7 +52,33 @@ const Home = () => {
                 >
                     Let's solve problems!
                 </button>
-                {/*<img src="/image/gradient.png" alt="Floating Gradient" className="floating-image"/>*/}
+
+                <img src={"/image/pageimage.png"} class="absolute top-[40%] right-[13%] w-[30%] h-auto pl-3"/>
+
+
+                {/*<div className="relative w-full h-full">*/}
+                {/*    {images.map((image, index) => (*/}
+                {/*        <img*/}
+                {/*            key={index}*/}
+                {/*            src={image}*/}
+                {/*            alt={`home-image-${index}`}*/}
+                {/*            className={`floating-image absolute left-100 top-40 transition-all duration-1000 ease-in-out ${*/}
+                {/*                currentImage >= index ? 'opacity-100 z-10' : 'opacity-0 z-0'*/}
+                {/*            }`}*/}
+                {/*            style={{*/}
+                {/*                transform:*/}
+                {/*                    currentImage === index*/}
+                {/*                        ? (index === 2*/}
+                {/*                            ? "translate(-10px, -20px)  scale(1.3)" // 맨 위 이미지 왼쪽 위로 이동 + 10% 크게*/}
+                {/*                            : index === 1*/}
+                {/*                                ? "translate(-10px, -20px) " // 두 번째 이미지 약간 왼쪽 이동*/}
+                {/*                                : "translate(-10px, -20px) rotate(0)") // 첫 번째 이미지는 정중앙*/}
+                {/*                        : "translateY(30px)",*/}
+                {/*                transition: 'opacity 1s ease-in-out, transform 1s ease-in-out',*/}
+                {/*            }}*/}
+                {/*        />*/}
+                {/*    ))}*/}
+                {/*</div>*/}
             </div>
             {/* <Footer /> */}
         </div>
