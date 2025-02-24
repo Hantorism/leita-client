493af6ec85a1c9834edf86195aad31fdea3499eb
tae1231 <sandy2528@naver.com>
2025-02-24 18:31:57 +0900

    css: 버튼 추가

--- src/components/common/CodeEditor.tsx
+++ src/components/common/CodeEditor.tsx
@@ -68,7 +68,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ code, setCode, problemId }) => 
     return (
         <div className="flex-1 min-w-[300px] min-h-[100px] bg-[#2A2A2A] p-6 rounded-lg shadow-lg m-4 flex flex-col">
             <div className="flex justify-between items-center">
-                <h2 className="text-xl font-semibold text-[#CAFF33]">코드 작성</h2>
+                {/*<h2 className="text-xl font-semibold text-[#CAFF33]">코드 작성</h2>*/}
 
                 <div className="flex items-center space-x-4">
                     <select
@@ -92,6 +92,13 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ code, setCode, problemId }) => 
                         disabled={isRunning}
                     >
                         {isRunning ? "RUNNING" : "RUN"}
+                    </button>
+                    <button
+                        onClick={handleRunCode}
+                        className="font-lexend px-[15px] py-[5px] text-[0.9rem] font-light text-[#1A1A1A] bg-[#9D5CE9] rounded-[80px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#9D5CE9] hover:to-[#CAFF33] hover:scale-[1.05] hover:text-white hover:shadow-[0px_4px_15px_rgba(202,_255,_51,_0.4)] text-left"
+                        disabled={isRunning}
+                    >
+                        Submit
                     </button>
                 </div>
             </div>
