3c57896a94b0eded0932b85eb1149fe766248d66
tae1231 <sandy2528@naver.com>
2025-04-08 16:25:01 +0900

    feat: 토큰 만료 로그아웃

---
+++ src/components/common/axiosInstance.js
@@ -0,0 +1,30 @@
+// src/api/axiosInstance.js
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 응답 인터셉터 추가
+instance.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      // 로그아웃 로직이 여기선 직접 접근 불가 -> 브라우저 리다이렉트
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      Cookies.remove("accessToken");
+      Cookies.remove("refreshToken");
+      window.location.href = "/login"; // 강제로 로그인 페이지로 이동
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
