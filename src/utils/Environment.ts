49561a68069e05d1e7e94ab909af60a4e1544490
zzzang12 <wadevs12@gmail.com>
2025-12-15 10:57:56 +0900

    feat: Profile 구분

--- src/utils/Environment.ts
+++ src/utils/Environment.ts
@@ -9,7 +9,7 @@ const getEnvVar = (name: string): string => {
 const Environment = {
 	GOOGLE_AUTH_CLIENT_ID: getEnvVar('REACT_APP_GOOGLE_AUTH_CLIENT_ID'),
 	API_BASE_URL:          getEnvVar('REACT_APP_API_BASE_URL'),
-	RUNTIME:               getEnvVar('REACT_APP_RUNTIME'),
+	PROFILE:               getEnvVar('REACT_APP_PROFILE'),
 };
 
 export default Environment;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ import AxiosInstance from './AxiosInstance';
 import Environment from './Environment';
 import EncodeBase64 from './EncodeBase64';
 import DecodeBase64 from './DecodeBase64';
+import Profile from './Profile';
 
 export {
 	Logger,
@@ -10,4 +11,5 @@ export {
 	Environment,
 	EncodeBase64,
 	DecodeBase64,
+	Profile,
 };
